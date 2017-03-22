3cbd5ccab1dec51a94a4da2e205db938759ed30a
y2k <itwisterlx@gmail.com>
2017-03-22 23:33:22 +0300

    .

--- Program.fs
+++ Program.fs
@@ -7,9 +7,12 @@ open SlackToTelegram.Messengers
 http://api.slackarchive.io/v1/messages?size=5&team=T09229ZC6&channel=C2X2LMYQ2&offset=0
 http://api.slackarchive.io/v1/messages?size=5&team=T09229ZC6&channel=C09222272&offset=0
 
-#r "../../.nuget/packages/system.reactive.core/3.1.1/lib/netstandard1.3/System.Reactive.Core.dll"
-#r "../../.nuget/packages/system.reactive.linq/3.1.1/lib/netstandard1.3/System.Reactive.Linq.dll"
-#r "../../.nuget/packages/telegram.bot/10.4.0/lib/netstandard1.1/Telegram.Bot.dll"
+#r "../../.nuget/packages/system.reactive.platformservices/3.1.1/lib/net45/System.Reactive.PlatformServices.dll"
+#r "../../.nuget/packages/system.reactive.interfaces/3.1.1/lib/net45/System.Reactive.Interfaces.dll"
+#r "../../.nuget/packages/system.reactive.core/3.1.1/lib/net45/System.Reactive.Core.dll"
+#r "../../.nuget/packages/system.reactive.linq/3.1.1/lib/net45/System.Reactive.Linq.dll"
+
+#r "../../.nuget/packages/telegram.bot/10.4.0/lib/net45/Telegram.Bot.dll"
 #r "../../.nuget/packages/newtonsoft.json/9.0.1/lib/net45/Newtonsoft.Json.dll"
 *)
 
@@ -19,10 +22,16 @@ let nowUtc () = DateTime.UtcNow.Subtract(DateTime(1970, 1, 1)).TotalSeconds
 let main argv =
     let token = argv.[0]
 
-    // Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
-    //     |> Observable.map (fun _ -> getNewBotMessages token 0)
-    //     |> Observable.subscribe (fun xs -> ())
-    //     |> ignore
+(*
+
+System.Reactive.Linq.Observable.Timer(System.TimeSpan.Zero, System.TimeSpan.FromSeconds(30.0))    
+
+*)
+
+    Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
+        |> Observable.map (fun _ -> getNewBotMessages token 0)
+        |> Observable.subscribe (fun xs -> ())
+        |> ignore
 
     Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
         |> Observable.map (fun _ -> 
