c0629203936383c4866915c79f031cde614c51ea
y2k <itwisterlx@gmail.com>
2017-03-24 17:39:30 +0300

    handle message

--- Program.fs
+++ Program.fs
@@ -23,6 +23,13 @@ System.Reactive.Linq.Observable.Timer(System.TimeSpan.Zero, System.TimeSpan.From
 
 let nowUtc () = DateTime.UtcNow.Subtract(DateTime(1970, 1, 1)).TotalSeconds
 
+let handle (message: string) = 
+    match message.Split(' ') |> Seq.toList with
+    | "list"::_      -> "List of subscriptions"
+    | "add"::x::_    -> "Add" + x
+    | "remove"::x::_ -> "Remove" + x
+    | _              -> "Commands: list, add <thread>, remove <thread>"
+
 [<EntryPoint>]
 let main argv =
     let token = argv.[0]
@@ -35,7 +42,7 @@ System.Reactive.Linq.Observable.Timer(System.TimeSpan.Zero, System.TimeSpan.From
 
     Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
         |> Observable.map (fun _ -> getNewBotMessages token 0)
-        |> Observable.subscribe (fun xs -> ())
+        |> Observable.subscribe (ignore)
         |> ignore
 
     Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
