8f0c04ed784c9abede64f7cef6cd9cd2338fd7fa
y2k <itwisterlx@gmail.com>
2017-03-27 02:12:06 +0300

    clean code

--- Program.fs
+++ Program.fs
@@ -8,21 +8,6 @@ open SlackToTelegram.Utils
 module bot = SlackToTelegram.Messengers
 module db  = SlackToTelegram.Storage
 
-(*
-http://api.slackarchive.io/v1/messages?size=5&team=T09229ZC6&channel=C2X2LMYQ2&offset=0
-http://api.slackarchive.io/v1/messages?size=5&team=T09229ZC6&channel=C09222272&offset=0
-
-#r "../../.nuget/packages/system.reactive.windows.threading/3.1.1/lib/net45/System.Reactive.Windows.Threading.dll"
-#r "../../.nuget/packages/system.reactive.platformservices/3.1.1/lib/net45/System.Reactive.PlatformServices.dll"
-#r "../../.nuget/packages/system.reactive.interfaces/3.1.1/lib/net45/System.Reactive.Interfaces.dll"
-#r "../../.nuget/packages/system.reactive.core/3.1.1/lib/net45/System.Reactive.Core.dll"
-#r "../../.nuget/packages/system.reactive.linq/3.1.1/lib/net45/System.Reactive.Linq.dll"
-
-#r "../../.nuget/packages/telegram.bot/10.4.0/lib/net45/Telegram.Bot.dll"
-#r "../../.nuget/packages/newtonsoft.json/9.0.1/lib/net45/Newtonsoft.Json.dll"
-
-*)
-
 let parseMessage (user: User) (message: string) = 
     match message.Split(' ') |> Seq.toList with
     | "list"::_      -> db.query user |> List.map (fun x -> "<code>" + x.id + "</code>")
