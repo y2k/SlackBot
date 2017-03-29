65ff838072200455b2188883f3f9170ba113465e
y2k <itwisterlx@gmail.com>
2017-03-29 23:38:41 +0300

    fixed: code format

--- Program.fs
+++ Program.fs
@@ -11,21 +11,21 @@ module o   = Observable
 
 let parseMessage (user: User) (message: string) = 
     match message.Split(' ') |> Seq.toList with
-    | "top"::_       -> bot.getSlackChannels () 
-                        |> List.filter (fun x -> x.num_members >= 100)
-                        |> List.sortByDescending (fun x -> x.num_members)
-                        |> List.map (fun x -> sprintf "• <b>%O</b> (%O) - %O" x.name x.num_members x.purpose.value) 
-                        |> List.reduce (fun a x -> a + "\n" + x)
-                        |> (+) "<b>Список доступных каналов:</b> \n"
-    | "ls"::_      -> match db.query user with
-                        | [] -> "У вас нет подписок. Добавьте командой: <code>add [канал]</code>"
-                        | channels -> channels |> List.sortBy (fun x -> x.id)
-                                               |> List.map (fun x -> "<code>" + x.id + "</code>")
-                                               |> List.reduce (fun a x -> a + ", " + x)
-                                               |> (+) "Каналы на которые вы подписаны: "
-    | "add"::x::_    -> db.add user x; "Подписка на <code>" + x + "</code> выполнена успешно"
-    | "rm"::x::_ -> db.remove user x; "Отписка от <code>" + x + "</code> выполнена успешно"
-    | _              -> "<b>Команды бота:</b>
+    | "top"::_    -> bot.getSlackChannels () 
+                     |> List.filter (fun x -> x.num_members >= 100)
+                     |> List.sortByDescending (fun x -> x.num_members)
+                     |> List.map (fun x -> sprintf "• <b>%O</b> (%O) - %O" x.name x.num_members x.purpose.value) 
+                     |> List.reduce (fun a x -> a + "\n" + x)
+                     |> (+) "<b>Список доступных каналов:</b> \n"
+    | "ls"::_     -> match db.query user with
+                     | [] -> "У вас нет подписок. Добавьте командой: <code>add [канал]</code>"
+                     | channels -> channels |> List.sortBy (fun x -> x.id)
+                                            |> List.map (fun x -> "<code>" + x.id + "</code>")
+                                            |> List.reduce (fun a x -> a + ", " + x)
+                                            |> (+) "Каналы на которые вы подписаны: "
+    | "add"::x::_ -> db.add user x; "Подписка на <code>" + x + "</code> выполнена успешно"
+    | "rm"::x::_  -> db.remove user x; "Отписка от <code>" + x + "</code> выполнена успешно"
+    | _           -> "<b>Команды бота:</b>
 • <b>top</b> - топ каналов kotlinlang.slack.com на которые можно подписаться
 • <b>ls</b> - список каналов kotlinlang.slack.com на которые вы подписаны
 • <b>add</b> [канал] - подписаться на обновления канала (пример: <code>add russian</code>)
