857b95ebca6884aa1166989afd9358a289b5c81e
y2k <itwisterlx@gmail.com>
2017-05-17 20:11:00 +0300

    refactoring

--- Program.fs
+++ Program.fs
@@ -9,41 +9,44 @@ module bot = SlackToTelegram.Messengers
 module db  = SlackToTelegram.Storage
 module o   = Observable
 
-let handleMessage (user: User) (message: string) = 
-    match message.Split(' ') |> Seq.toList with
-    | "top"::_    -> bot.getSlackChannels () 
-                     |> List.filter (fun x -> x.num_members >= 100)
-                     |> List.sortByDescending (fun x -> x.num_members)
-                     |> List.map (fun x -> sprintf "• <b>%O</b> (%O) - %O" x.name x.num_members x.purpose.value) 
-                     |> List.reduce (fun a x -> a + "\n" + x)
-                     |> (+) "<b>Список доступных каналов:</b> \n"
-    | "ls"::_     -> match db.query user with
-                     | [] -> "У вас нет подписок. Добавьте командой: <code>add [канал]</code>"
-                     | channels -> channels |> List.sortBy (fun x -> x.id)
-                                            |> List.map (fun x -> "<code>" + x.id + "</code>")
-                                            |> List.reduce (fun a x -> a + ", " + x)
-                                            |> (+) "Каналы на которые вы подписаны: "
-    | "add"::x::_ -> db.add user x; "Подписка на <code>" + x + "</code> выполнена успешно"
-    | "rm"::x::_  -> db.remove user x; "Отписка от <code>" + x + "</code> выполнена успешно"
-    | _           -> "<b>Команды бота:</b>
-• <b>top</b> - топ каналов kotlinlang.slack.com на которые можно подписаться
-• <b>ls</b> - список каналов kotlinlang.slack.com на которые вы подписаны
-• <b>add</b> [канал] - подписаться на обновления канала (пример: <code>add russian</code>)
-• <b>rm</b> [канал] - отписаться от канал (пример: <code>remove russian</code>)"
+module Domain =
+    let handleMessage (user: User) (message: string) = 
+        match message.Split(' ') |> Seq.toList with
+        | "top"::_    -> bot.getSlackChannels () 
+                        |> List.filter (fun x -> x.num_members >= 100)
+                        |> List.sortByDescending (fun x -> x.num_members)
+                        |> List.map (fun x -> sprintf "• <b>%O</b> (%O) - %O" x.name x.num_members x.purpose.value) 
+                        |> List.reduce (fun a x -> a + "\n" + x)
+                        |> (+) "<b>Список доступных каналов:</b> \n"
+        | "ls"::_     -> match db.query user with
+                        | [] -> "У вас нет подписок. Добавьте командой: <code>add [канал]</code>"
+                        | channels -> channels |> List.sortBy (fun x -> x.id)
+                                                |> List.map (fun x -> "<code>" + x.id + "</code>")
+                                                |> List.reduce (fun a x -> a + ", " + x)
+                                                |> (+) "Каналы на которые вы подписаны: "
+        | "add"::x::_ -> db.add user x; "Подписка на <code>" + x + "</code> выполнена успешно"
+        | "rm"::x::_  -> db.remove user x; "Отписка от <code>" + x + "</code> выполнена успешно"
+        | _           -> "<b>Команды бота:</b>
+    • <b>top</b> - топ каналов kotlinlang.slack.com на которые можно подписаться
+    • <b>ls</b> - список каналов kotlinlang.slack.com на которые вы подписаны
+    • <b>add</b> [канал] - подписаться на обновления канала (пример: <code>add russian</code>)
+    • <b>rm</b> [канал] - отписаться от канал (пример: <code>remove russian</code>)"
+
+    let filterChannels (dbChannels, channels) =
+        channels |> List.where (fun x -> dbChannels |> List.contains x.name)
 
 [<EntryPoint>]
 let main argv =
     let token = argv.[0]
 
     bot.getNewBotMessages token
-        |> o.map (fun x -> (x.user, x.text |> handleMessage x.user))
+        |> o.map (fun x -> (x.user, x.text |> Domain.handleMessage x.user))
         |> o.map (fun (user, response) -> bot.sendToTelegramSingle token user Styled response)
         |> (fun o -> o.Subscribe(DefaultErrorHandler())) |> ignore
 
     Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(30.))
-        |> o.map (fun _ -> 
-            let dbChannels = db.getAllChannels ()
-            bot.getSlackChannels () |> List.where (fun x -> dbChannels |> List.contains x.name))
+        |> o.map (fun _ -> (db.getAllChannels(), bot.getSlackChannels()))
+        |> o.map Domain.filterChannels
         |> flatMap (fun x -> x.ToObservable())
         |> flatMap (fun ch -> 
             let channelOffset = db.getOffsetWith ch.name |> Option.defaultValue "0"
