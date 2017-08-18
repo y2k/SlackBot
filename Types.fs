9d1b30bfc91ac0d480eb5002a999d8dcf940d6d3
y2k <itwisterlx@gmail.com>
2017-08-18 14:39:44 +0300

    format code

--- Types.fs
+++ Types.fs
@@ -1,14 +1,27 @@
 namespace SlackToTelegram
+
 open System
 
 type TelegramOffset = string
+
 type User = string
+
 type Token = string
+
 type ChannelId = string
 
-type Channel = { id: ChannelId; user: User }
-type SlackMessage = { text: string; user: string; ts: string }
-type TextMode = | Plane | Styled
+type Channel = 
+    { id : ChannelId
+      user : User }
+
+type SlackMessage = 
+    { text : string
+      user : string
+      ts : string }
+
+type TextMode = 
+    | Plane
+    | Styled
 
 type DefaultErrorHandler() = 
     interface IObserver<unit> with
@@ -16,14 +29,16 @@ type DefaultErrorHandler() =
         member this.OnNext(s) = printfn "Status OK (%O)" DateTime.Now
         member this.OnCompleted() = ()
 
-module Utils =
+module Utils = 
     open System
     open System.Collections.Generic
     open System.Reactive.Linq
-
-    let tryGet (dict: Dictionary<'k, 'v>) (key: 'k) =
+    
+    let tryGet (dict : Dictionary<'k, 'v>) (key : 'k) = 
         let (success, value) = dict.TryGetValue(key)
-        if (success) then Some value else None
-
-    let nowUtc () = DateTime.UtcNow.Subtract(DateTime(1970, 1, 1)).TotalSeconds
-    let flatMap (f: 'a -> IObservable<'b>) (o: IObservable<'a>) = o.Select(f).Merge(3)
+        if (success) then Some value
+        else None
+    
+    let nowUtc() = DateTime.UtcNow.Subtract(DateTime(1970, 1, 1)).TotalSeconds
+    let flatMap (f : 'a -> IObservable<'b>) (o : IObservable<'a>) = 
+        o.Select(f).Merge(3)
